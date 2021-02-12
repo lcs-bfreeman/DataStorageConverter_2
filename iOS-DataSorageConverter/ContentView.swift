eaed98206275eb927b8384b7c5b75c88ba170d6d
Benjamn <bfreeman@lcs.on.ca>
2021-02-12 11:24:41 -0500

    Example of using a stored property and a computed property

---
+++ iOS-DataSorageConverter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-DataSorageConverter
+//
+//  Created by Ben Freeman on 2021-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+var body: some View {
+    Text (greetin)
+        .padding()
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-DataSorageConverter/iOS_DataSorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataSorageConverterApp.swift
+//  iOS-DataSorageConverter
+//
+//  Created by Ben Freeman on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataSorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
