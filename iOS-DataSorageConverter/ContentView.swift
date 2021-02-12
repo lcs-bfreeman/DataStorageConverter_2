869ada3cc57702929ea40fdf8ecada2df5e927ad
Benjamn <bfreeman@lcs.on.ca>
2021-02-12 11:59:33 -0500

    Added a picker for the from data storage unit

--- iOS-DataSorageConverter/ContentView.swift
+++ iOS-DataSorageConverter/ContentView.swift
@@ -1,33 +1,60 @@
 //
 //  ContentView.swift
-//  iOS-DataSorageConverter
+//  iOS-DataStorageConverter
 //
-//  Created by Ben Freeman on 2021-02-12.
+//  Created by Russell Gordon on 2021-02-12.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     
-    // MARK: store properties
+    // MARK: Stored properties
+    @State private var fromUnit: DataStorageUnit = .bit
     @State private var input: String = ""
-    
-//MARK:  Computed properties
+    @State private var toUnit: DataStorageUnit = .bit
+
+    // MARK: Computed properties
     private var output: String {
-        return "You typed in \(input)"
+        
+        // Ensure the provided input can be expressed as an integer
+        guard let providedValue = Int(input) else {
+            return "Please provide an integer value."
+        }
+
+        // Return the converted value
+        return convert(providedValue: providedValue, fromUnit: fromUnit, toUnit: toUnit)
+
     }
     
-    
-var body: some View {
-    
-    Form {
+    var body: some View {
         
-        TextField("e.g: 1024", text: $input)
-            .keyboardType(.numberPad)
-    
+        Form {
+            Picker("From unit:", selection: $fromUnit) {
+                Text(DataStorageUnit.bit.rawValue).tag(DataStorageUnit.bit)
+                Text(DataStorageUnit.nibble.rawValue).tag(DataStorageUnit.nibble)
+                Text(DataStorageUnit.byte.rawValue).tag(DataStorageUnit.byte)
+                Text(DataStorageUnit.kilobyte.rawValue).tag(DataStorageUnit.kilobyte)
+                Text(DataStorageUnit.megabyte.rawValue).tag(DataStorageUnit.megabyte)
+                Text(DataStorageUnit.gigabyte.rawValue).tag(DataStorageUnit.gigabyte)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            TextField("e.g.: 1024", text: $input)
+                .keyboardType(.numberPad)
+            
+            Picker("To unit:", selection: $toUnit) {
+                Text(DataStorageUnit.bit.rawValue).tag(DataStorageUnit.bit)
+                Text(DataStorageUnit.nibble.rawValue).tag(DataStorageUnit.nibble)
+                Text(DataStorageUnit.byte.rawValue).tag(DataStorageUnit.byte)
+                Text(DataStorageUnit.kilobyte.rawValue).tag(DataStorageUnit.kilobyte)
+                Text(DataStorageUnit.megabyte.rawValue).tag(DataStorageUnit.megabyte)
+                Text(DataStorageUnit.gigabyte.rawValue).tag(DataStorageUnit.gigabyte)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+
             Text(output)
-    }
-        
+        }
     }
 }
 
@@ -36,4 +63,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
