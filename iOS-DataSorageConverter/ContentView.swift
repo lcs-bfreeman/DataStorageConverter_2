4e9065fb9cf23ece685076b2c1c8ed902de3776d
Benjamn <bfreeman@lcs.on.ca>
2021-02-12 11:47:25 -0500

    ADDED PICKER

--- iOS-DataSorageConverter/ContentView.swift
+++ iOS-DataSorageConverter/ContentView.swift
@@ -11,15 +11,25 @@ struct ContentView: View {
     
     @State private var name: String = ""
     
+    @State private var feeling: String = ""
+    
+    
     private var greeting: String {
-        return "Hello, \(name)"
+        return "Hello, \(name), you are \(feeling)"
     }
 
 var body: some View {
     
-    VStack {
+    Form {
         
         TextField("Enter your name", text: $name)
+        // In a picker it displays the Text and stores the .tag
+        Picker("Mood", selection: $feeling) {
+            Text("😀").tag("Happy")
+            Text("😞").tag("Sad")
+            Text("😐").tag("Fine")
+        }
+        .pickerStyle(SegmentedPickerStyle())
         
         Text(greeting)
             .padding()
